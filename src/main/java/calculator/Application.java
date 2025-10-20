225009b2dac259b561e488231849931f0cb7aee0
seobeeeee1001 <wjsbdcindsu@pukyong.ac.kr>
2025-10-20 14:14:27 +0900

    feature(application) : 커스텀 구분자 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -11,6 +11,11 @@ public class Application {
             return;
         }
         String delimeter = "[,:]";
+        if(input.startsWith("//")) {
+            int newLineIndex = input.indexOf("\\n");
+            delimeter = input.substring(2, newLineIndex);
+            input = input.substring(newLineIndex + 2);
+        }
         String[] numbers = input.split(delimeter);
         int sum = 0;
         for(String number : numbers) {
